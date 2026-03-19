1da64f242a9615f5146687b9c99cb566900b8b0a
2026proton23000 <peru_2026.23000@proton.me>
2026-03-18 19:13:50 -0500

    ci: config site, base and gh-pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  // 1. Ton URL GitHub Pages (sans le nom du repo)
+  site: 'https://2026proton23000.github.io',
+  // 2. Le nom de ton dépôt GitHub précédé d'un slash
+  base: '/hexagone', 
   integrations: [tailwind()],
   i18n: {
     defaultLocale: "fr",
